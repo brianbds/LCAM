4e48b4d4473e0456a623683164c36c33151ef69d
Branislav Budoš <69458986+brianbds@users.noreply.github.com>
2020-09-25 21:46:00 +0200

    finished rgb to lum scripts

--- Visibility/RGB_to_lum_for_strings.m
+++ Visibility/RGB_to_lum_for_strings.m
@@ -1,5 +1,20 @@
+%This script is used for computing luminance valu over 3 stripes where only
+%the 2nd stripe is selected in a box and the TOP/BOTTOM stripe is boxed
+%automatically
+%   /´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´\                 /´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´\                                                                                                                        
+%  |                                                        |                |                                                        |                                                                 
+%  |             ||||||||||||||||||||             |                |      1      ||||||||||||||||||||        3   |                                                                                             
+%  |                                                        |    ==>    |        XˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇX  |                                                                        
+%  |              ||||||||||||||||||||            |                |       |       ||||||||||||||||||||      |   |                                                                                             
+%  |                                                        |                |       X________________________X  |                                                                
+%  |             ||||||||||||||||||||||          |                |      2     ||||||||||||||||||||||     4   |
+%  \_______________________________/                  \_______________________________/                                                                                                                      
+
 clear
-i=1; samples = 50;
+samples = 50; % Number of samples
+RGBMultiplier=[0.2126,0.7152,0.0722]; % RGB multiplying values
+
+i=1; 
 while 1==1
     [fi,pa]= uigetfile('MultiSelect','on','*.jpg');
     if str2double(string(pa))==0; break; end
@@ -11,6 +26,7 @@ while 1==1
     else
         figure,imshow(I,[])
     end
+     % Selecting corners on a picture
     [coor(1,2),coor(1,1)] = ginput(1);
     hold on;
     plot(coor(1,2),coor(1,1),'r+', 'MarkerSize', 25);
@@ -22,28 +38,25 @@ while 1==1
     plot(coor(4,2),coor(4,1),'b+', 'MarkerSize', 25);
     close all
     Acoor(:,:,i)=floor(coor);
-    
     files{i}=fi;path(i)=string(pa);clear fi pa; i=i+1;
 end
 clear I
-RGBMultiplier=[0.2126,0.7152,0.0722];
 g=length(path);
 lum=zeros([samples,g*3]);
 for k=1:g
-    s=size(files{1,g});
+    s=size(files{1,k});
     if s<samples
-        files{1,g}(end+1:samples)=files{1,g}(end);
+        files{1,k}(end+1:samples)=files{1,k}(end);
     end
-    coor=sortrows(Acoor(:,:,g));
+    % Sorting coordinate in order to have corners 1-4 exatcly in positions 1-4
+    coor=sortrows(Acoor(:,:,k));
     coor([1,2],:)=sortrows(coor([1,2],:),2);
     coor([3,4],:)=sortrows(coor([3,4],:),2);
     clear l
     t=max(coor(:,1)-min(coor(:,1)))<max(coor(:,2)-min(coor(:,2)));
-    
     [coor(:,:,1),coor(:,:,2),coor(:,:,3)]=deal(coor);
-    
-    
     if t
+        %Extending coordinates UP and DOWN in order to read stripes
         d=floor((coor(3,1,2)-coor(1,1,2)+coor(4,1,2)-coor(2,1,2))/2);
         
         coor(1,:,1)=[floor((coor(1,1,1)+coor(2,1,1))/2-d), floor((coor(1,2,1)+coor(3,2,1))/2)];
@@ -67,6 +80,7 @@ for k=1:g
             pic=rot90(pic);
         end
         for o=1:3
+            %Core processing loop
             picture=double(pic(coor(1,1,o):coor(2,1,o),coor(1,2,o):coor(2,2,o),:))./255;
             if i==1;imshow(picture.*255);pause(2);close all;end
             picture(picture<=0.04045)=picture(picture<=0.04045)./12.92;
--- Visibility/RGB_to_luminance.m
+++ Visibility/RGB_to_luminance.m
@@ -1,5 +1,21 @@
+%This script is used for computing luminance valu over one big patch of a
+%cloth by selecting all files, writing number of samples and if necessary
+%changing the multiplier values
+%   /´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´\                 /´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´\                                                                                                                        
+%  |                                                        |                |     1 XˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇX 4 |                                                                 
+%  |             ||||||||||||||||||||             |                |       |      ||||||||||||||||||||      |    |                                                                                             
+%  |              ||||||||||||||||||||            |    ==>    |       |       ||||||||||||||||||||      |    |                                                                        
+%  |              ||||||||||||||||||||            |                |       |      ||||||||||||||||||||      |    |                                                                                             
+%  |              |||||||||||||||||||||          |                |       |        |||||||||||||||||||||   |    |                                                                
+%  |                                                        |                |    2 X_______________________X  3|
+%  \_______________________________/                  \_______________________________/                                                                                                                      
+
 clear 
-i=1;samples = 50;
+
+samples = 50; % Number of samples
+RGBMultiplier=[0.2126,0.7152,0.0722]; % RGB multiplying values
+
+i=1;
 while 1==1
     [fi,pa]= uigetfile('MultiSelect','on','*.jpg');
     if str2double(string(pa))==0; break; end
@@ -11,6 +27,7 @@ while 1==1
     else
         figure,imshow(I,[])
     end
+    % Selecting corners on a picture
     [coor(1,2),coor(1,1)] = ginput(1);
     hold on;
     plot(coor(1,2),coor(1,1),'r+', 'MarkerSize', 25);
@@ -22,11 +39,9 @@ while 1==1
     plot(coor(4,2),coor(4,1),'b+', 'MarkerSize', 25);
     close all
     Acoor(:,:,i)=floor(coor);
-    
     files{i}=fi;path(i)=string(pa);clear fi pa; i=i+1;
 end
 clear I
-RGBMultiplier=[0.2126,0.7152,0.0722];
 lum=zeros([samples,1]);
 g=length(path);
 for k=1:g
@@ -34,12 +49,13 @@ for k=1:g
     if s(2)<samples
         files{1,k}(end+1:samples)=files{1,k}(end);
     end
+    % Sorting coordinate in order to have corners 1-4 exatcly in positions 1-4
     coor=sortrows(Acoor(:,:,g));
     coor([1,2],:)=sortrows(coor([1,2],:),2);
     coor([3,4],:)=sortrows(coor([3,4],:),2);
     coor=[floor(mean(coor([1,2],1))) ,floor(mean(coor([1,3],2))) ;floor(mean(coor([3,4],1))) ,floor(mean(coor([2,4],2)))];
-    
     for i=1:samples
+        %Core processing loop
         picture=double(imread(char(fullfile(path(k),files{k}(i)))))./255;
         [r,c,~]=size(picture);
         if r>c;picture=rot90(picture);end
@@ -52,5 +68,6 @@ for k=1:g
         lum(i,k)=sum(sum(picture));
     end
 end
+path=path(:);
 plot(lum)
 clear N i filename files luminance picture RGBMultiplier Acoor c coor fi g k pa r s
